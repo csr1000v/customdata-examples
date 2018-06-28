cd71a2b5749536ebb62dd01882b3584fddf62d43
vishleshp <vishlpa4@cisco.com>
2018-06-28 01:01:39 -0400

    commit scripts

---
+++ scripts/packetDropsReporter.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import os
+import datetime
+import time
+
+FORMAT = '%(asctime)20s:%(module)15s:%(funcName)20s:%(lineno)4s - %(levelname)7s - %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('smartLicensingConfigurator')
+logger.setLevel(logging.DEBUG)
+
+try:
+    import cli
+except Exception as e:
+    logger.warning("not able to import cli module")
+    logger.exception(e)
+
+def report_drops(entries):
+    home = os.path.expanduser('~')
+    output_dir = home + "dropsReporterOutput"
+    try:
+        os.makedirs(output_dir)
+    except Exception as e:
+        logger.exception(e)
+
+    output_file = output_dir + "/drops_report.txt"
+    curtime = datetime.datetime.now()
+
+    with open(output_file, 'wa') as f:
+        f.write( str(curtime) + output_file )
+
+def execute_command(command):
+    cmd_output = cli.execute(command)
+    while len(cmd_output) == 0:
+        logger.warning("CMD FAILED, retrying")
+        cmd_output = cli.execute(command)
+    return cmd_output
+
+def get_stat_drop():
+    for i in range(2):
+        try:
+            cmd_output = execute_command(
+                "show platform hardware qfp active statistics drop clear")
+
+            for line in cmd_output.splitlines():
+                if ("-" in line) or ("Global Drop Stats" in line):
+                    continue
+
+                entries = line.split()
+                output_line = "%s --> %s/%s" % (entries[0], entries[1], entries[2])
+                logger.info(output_line)
+                report_drops(output_line)
+                break
+        except Exception as e:
+            logger.exception(e)
+            pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="packets drops reporter script that reports packets drops")
+
+    parser.add_argument('--drops', action='store_true', default=True,
+                        help='capture drops from show platform hardware qfp active statistics drop clear command')
+
+    args = parser.parse_args()
+    while args.drops:
+        get_stat_drop()
+        # sleep for 5 minutes
+        print("in here")
+        time.sleep(300)
---
+++ scripts/smartLicensingConfigurator.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+
+FORMAT = '%(asctime)20s:%(module)15s:%(funcName)20s:%(lineno)4s - %(levelname)7s - %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('smartLicensingConfigurator')
+logger.setLevel(logging.DEBUG)
+try:
+    import cli
+except Exception as e:
+    logger.warning("failed to import cli module")
+
+def configure_smart_licensing(idtoken, throughput):
+    if idtoken is None:
+        logger.warning("idtoken value is None. Please provide valid idtoken")
+        return False
+
+    email='sch-smart-licensing@cisco.com'
+    smart_licensing_configuration = '''
+    service call-home
+    call-home
+    contact-email-addr {}
+    profile "CiscoTAC-1"
+    active
+    destination transport-method http
+    no destination transport-method email
+    destination address http https://tools.cisco.com/its/service/oddce/services/DDCEService
+    
+    license smart enable
+    do license smart register idtoken {}
+    platform hardware throughput level MB {}
+    '''.format(idtoken, throughput, email)
+    logger.info("Trying to configure smart licensing. Configs: {}".format(smart_licensing_configuration))
+    cli.configure(smart_licensing_configuration)
+    return True
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script to Configure Smart licensing on CSR")
+    parser.add_argument('--idtoken', action='store', dest='idtoken',
+                        help='provide account idtoken string')
+    parser.add_argument('--throughtput', help='provide desired throughtput level in MB. default is 2500', action='store',
+                        dest='throughput', default='2500')
+    args = parser.parse_args()
+    logger.info("idtoken: {}".format(args.idtoken))
+    logger.info("throughput: {}".format(args.throughput))
+    configure_smart_licensing()
