d0d7045a7287636d1543be454a7f0aeebfc4925a
Avani Vyas <avvyas@cisco.com>
2019-02-25 16:48:39 -0500

    Rearranging args

--- scripts/smartLicensingConfigurator.py
+++ scripts/smartLicensingConfigurator.py
@@ -12,7 +12,7 @@ try:
 except Exception as e:
     logger.warning("failed to import cli module")
 
-def configure_smart_licensing(idtoken, throughput, email):
+def configure_smart_licensing(email, idtoken, throughput):
     if idtoken is None:
         logger.warning("idtoken value is None. Please provide valid idtoken")
         return False
@@ -37,16 +37,17 @@ def configure_smart_licensing(idtoken, throughput, email):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to Configure Smart licensing on CSR")
+    parser.add_argument('--email',
+                        help='provide email address for smart licensing. default is sch-smart-licensing@cisco.com',
+                        action='store',
+                        dest='email', default='sch-smart-licensing@cisco.com')
     parser.add_argument('--idtoken', action='store', dest='idtoken', 
                         help='provide account idtoken string')
     parser.add_argument('--throughput', help='provide desired throughtput level in MB. default is 2500',
                         action='store',
                         dest='throughput', default='2500')
-    parser.add_argument('--email', help='provide email address for smart licensing. default is sch-smart-licensing@cisco.com',
-                        action='store',
-                        dest='email', default='sch-smart-licensing@cisco.com')
     args = parser.parse_args()
     logger.info("idtoken: {}".format(args.idtoken))
     logger.info("throughput: {}".format(args.throughput))
     logger.info("email: {}".format(args.email))
-    configure_smart_licensing(args.idtoken, args.idtoken, args.email)
+    configure_smart_licensing(args.email, args.idtoken, args.idtoken)
